d031d7c23703160f4aae9e6298a3e91ec617323f
Francileudo Oliveira <sfrancileudo1@gmail.com>
2022-05-18 18:30:32 -0300

    exercícios em javascript e typescript

---
+++ exercicios-simples-javascript/media-final.js
@@ -0,0 +1,10 @@
+// 2) Faça um programa que entregue a média final de 4 notas
+
+let nota1 = 10.0
+let nota2 = 5.0
+let nota3 = 7.0
+let nota4 = 8.0
+
+let mediaFinal = (nota1 + nota2 + nota3 + nota4) / 4
+
+console.log(mediaFinal)
